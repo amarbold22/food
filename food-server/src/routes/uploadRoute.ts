5f1619defb1d643ba10722073412e7d2e0135e0f
Амарболд Батболд <LP230101@G301-CC0192.local>
2024-03-20 18:43:35 +0800

    uploadRoute

--- food-server/src/routes/uploadRoute.ts
+++ food-server/src/routes/uploadRoute.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
 import multer from "multer";
 import { uploadFile } from "../controller/uploadController";
+import { upload } from "../utils/multer";
 
 const router = Router();
-const upload = multer({
-    dest: "./uploads"
-});
-
 router.route("/").post(upload.single("image"), uploadFile);
 
 export default router;
